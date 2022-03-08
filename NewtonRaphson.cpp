2c6a37525832ba1340f36799d65876b07c3d79d1
Kshitiz Wagle <kshitizbca076@ojashwicollege.edu.np>
2022-03-08 16:36:02 +0545

    fixed iterator

--- NewtonRaphson.cpp
+++ NewtonRaphson.cpp
@@ -3,16 +3,16 @@
 #define f(x) (sin(x) - x * x)
 #define g(x) (cos(x) - 2 * x)
 int main() {
-    int itr, max;
+    int max;
     float h, x0, x1, err;
     printf("Enter x0, allowed error and max. iterations: ");
     scanf("%f %f %d", &x0, &err, &max);
-    for (int i = 1; i <= max; ++i) {
+    for (int i = 0; i < max; ++i) {
         h = f(x0) / g(x0);
         x1 = x0 - h;
-        printf("At iteration no %d, x = %9.6f\n", itr, x1);
+        printf("At iteration no %d, x = %9.6f\n", i+1, x1);
         if (fabs(h) < err) {
-            printf("After %d iterations, root = %.6f\n", itr, x1);
+            printf("After %d iterations, root = %.6f\n", i+1, x1);
             return 0;
         }
         x0 = x1;
